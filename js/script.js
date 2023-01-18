c2c8114029922c2dbb8b350a0966e4b7c9c09a68
pub <tjrgh538@naver.com>
2023-01-18 15:41:21 +0900

    132

---
+++ js/script.js
@@ -0,0 +1,82 @@
+$(document).ready(function () {
+
+
+    time();
+
+
+});
+
+
+
+function time() {
+
+    
+
+    $('table tbody td:nth-child(4)').addClass("call");
+	
+
+	$("button").click(function(event) {
+ 
+        var i = $(this).index();  
+        console.log(i);  // 0번째
+ 
+        // good
+        var j = $("button").index(this);  // 존재하는 모든 버튼을 기준으로 index
+        console.log(j);
+        
+    });
+ 
+
+
+	$(".call").each(function() {
+		var idx = $(".call").index(this);
+		
+		var text = $(".call").eq(idx).text();
+		var findString = "20";
+
+		console.log(idx);
+
+
+		if(text.indexOf(findString) != 0) { /*존재하지 않으면 */
+			$(".pic").eq(idx).hide();
+		}
+		
+	
+	});
+		
+
+
+		
+
+
+	
+
+
+
+	
+
+
+
+	var telValue = $(".ced").val();
+	if ( telValue.match("dd")) {
+		alert('-을 제거해주세요.');
+		return false;
+	}
+
+
+
+	// var content = $(".prostatus .bn-list-common table tbody td").text()
+	
+	// // 글자수 세기
+	// if(call == '-'){
+	// 	alert("존재함")
+	// }else{
+	// 	alert("존재하지 않음")
+	// }
+	
+	
+
+
+    
+
+}
