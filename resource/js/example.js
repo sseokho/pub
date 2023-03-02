08c62040eeeb4d08e7d9c482ebe6cb991e73300d
pub <tjrgh538@naver.com>
2023-03-02 18:10:51 +0900

    456

--- resource/js/example.js
+++ resource/js/example.js
@@ -1,11 +1,33 @@
 $(document).ready(function () {
     se1Box1_Swiper();
+    se1Box2_Swiper();
 
 });    
 
 
 function se1Box1_Swiper(){
-
+    $('.hg_btn').hover(function() {
+        $(this).addClass("aa")
+      }, function(){
+        $(this).removeClass("aa")
+      });
+ 
 }  
-
-
+function se1Box2_Swiper(){
+    var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
